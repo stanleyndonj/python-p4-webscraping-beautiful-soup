d2e9d6f4f3055b48ab93a5256a21afccce0a6c8d
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-12-16 21:18:29 +0300

    done

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,16 @@
-from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+doc = BeautifulSoup(html.text, 'html.parser')
+
+# Extract a specific heading
+heading = doc.select('.heading-financier')
+print(heading[0].contents[0].strip())
+
+# Extract course titles
+courses = doc.select('.heading-60-black.color-black.mb-20')
+
+for course in courses:
+    print(course.contents[0].strip())
